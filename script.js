413c652cfa7642ce782b700b6f1ec743540ccdf1
8g8bdj5ct8-rgb <8g8bdj5ct8@privaterelay.appleid.com>
2026-01-15 11:29:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ window.onload = () => {
 };
 
 const slides = [
-    { text: "Jujur, cari alamat pondokmu kemarin itu tidak mudah. Harus mutar-mutar tanya sana-sini. Tapi niat saya memang mau antar amanah ini sendiri.", gif: "slide1.GIF" },
-    { text: "Berhenti bilang kalau kamu itu 'banyak minusnya'. Di mata saya, kamu tetap Princes yang berharga. Tolong jangan rendah diri lagi, ya?", gif: "slide2.GIF" },
-    { text: "Karena bagiku, bisa mengenalmu dan mengantar amanah ini sampai ke tanganmu sudah jadi kebahagiaan tersendiri buat saya.", gif: "slide3.GIF" },
-    { text: "Ini ada sedikit kenang-kenangan, sebuah Tumbler. Bukan sogokan, tapi apresiasi saya buat kamu. Mau diterima, kan?", gif: "slide4.GIF", isLast: true }
+    { text: "Ciee, masa Incess tdk na tau nama kontaknya di HP-ku? Ndak mungkinlah 😜. Anyway, I came here purely with niat and nekat. 😅 Bermodalkan ituji yg kita bilang pas telponanki: 'lewat SPBU, poros kota ke bira, terus samping pondok pesantren'. Just that, but with a good heart of course. ✨.", gif: "slide1.GIF" },
+    { text: "Hehehe, I’m sorry before. Pertama, krna saya ke sini nda bilangka atau izinka dulu, I should've told you first. 🙏🏻😅 Kedua, soal yg kemarin... I'm really sorry for that. Sdrka itu kmrin blunder besar ku dan bikin ki mrasa tdk nyaman atau risih dngn saya. It was never my intention, and I promise it won't happen again. 🤙🏻", gif: "slide2.GIF" },
+    { text: "Minta tolongka 🙏🏻 please stop thinking that you’re not worth it. Ingat, Incess itu “Princess in my eyes”. you don't deserve to feel that way. 😌 Menurutku toh, Incess pantas diperjuangkan oleh lelaki yg bisa menerima apa adanya dan semua kekuranganta. So, please no more negative thoughts, okay? 🧸", gif: "slide3.GIF" },
+    { text: "Nah, ini ada hadiah. I hope you like it and it's useful for you. 🎁 bkn sogokan ini nah, camkan itu. ku pilih ini krna mnrtku ini bermanfaat buat Incess, supaya Incess rajin minum air putih dan sehat terus. I’ve been planning to give this for our first meeting, jdi tlong disimpan baik-baik ya. I'll just let my actions prove everything. 🫡", gif: "slide4.GIF", isLast: true }
 ];
 
 let currentSlide = 0;
@@ -67,13 +67,13 @@ function nextSlide() {
         const btnLanjut = document.getElementById('btn-lanjut');
         const btnTolak = document.getElementById('btn-tolak');
 
-        textElem.innerHTML = "Amanah lunas. Jaga kesehatan baik-baik di sana. Saya balik ke Barru dulu. Take care, Princes.";
+        textElem.innerHTML = "Akhir kata, thanks for reading this. Minta tlong terima hadiahku nah. Soal kemarin, I’m really sorry once again. Saya hrp sampai di slide ini, Incess tdk merasa 'minus' lagi sebagai perempuan. You are worth it. 🩷 smoga ini awal hal baik untuk kita berduanah, wkwkwk.Tadi pas di perjalanannnah, jujur senang sekalika sampai ke bulkum tampa bantuan orang lain.See you when I see you, Princess. 👸🏻 Apapun keputusanta nantinya, ku terima semuaji. Saya mengerti keadaanta dan traumata yg pernah di cerita itu hari. Mungkin ini terlalu cepat bagi kita toh? I won't talk too much, I'll just prove it to you. 👋🏻 Bye-bye Incess, stay healthy! 🥤 dan ttp jadi Incess yg prtma kali ku kenal nah. ";
         gifElem.src = "slide5.GIF";
         btnLanjut.style.display = "none";
         btnTolak.style.display = "none";
 
         setTimeout(() => {
-            alert("Amanah lunas. Jaga kesehatan baik-baik di sana. Saya balik ke Barru dulu. Take care, Princes.");
+            alert("Mission accomplished! 🚩 Skrng saya balik ke Barru dulu ya");
         }, 500);
     }
 }
